44dcbc8e20bf02ab58be5d5909170ee3e9bb3815
Louis Neubauer <louisappleid9501@gmail.com>
2021-08-09 15:32:07 +0200

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,19 +13,15 @@ class GamesController < ApplicationController
     incl = include
 
     if is_english && incl
-      @check = params[:letters]
       @result = "Congratulations! #{@word.upcase} is an English word and is included in the grid!"
       @score = @word.length
     elsif is_english
-      @check = params[:letters]
       @result = "Congratulations! #{@word.upcase} is an English word! But does is not included in the grid: #{params[:letters]}"
       @score = 0
     elsif incl
-      @check = params[:letters]
       @result = "Congratulations! #{@word.upcase} is in the grid! But does is not an English word."
       @score = 0
     else
-      @check = params[:letters]
       @result = "Sorry but #{@word.upcase} does not seem to be a vaid English word nor in the grid..."
       @score = 0
     end
